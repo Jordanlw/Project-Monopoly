2aa8e16ea29c1d68f6feba1cf915a93a145879f5
Jordan W <jordanw2@gmail.com>
2011-01-14 21:56:24 +1030

    Finally fixed trade(), tested and complete.

--- Monopoly.c
+++ Monopoly.c
@@ -208,10 +208,19 @@ void trade(int current,struct player **players,struct property **properties,int 
 				int selPla2 = 0;
 				int range[2] = {1,amntPlayers};
 				parseInput(1,NULL,&selPla2,NULL,range,NULL,NULL);
-				printf("%s, Do you want to accept the trade?\n%s is giving %s, in return taking %d"
+				selPla2--;
+				printf("%s, Do you want to accept the trade?\n%s is giving property %s, in return taking $%d\n"
 				,(players[selPla2])->id,(players[current])->id,(properties[select])->name,amount);
-				(players[selPla2-1])->money -= amount;
-				(properties[select])->owner = selPla2-1;
+				char YaN[2] = {'y','n'};
+				int	YaNResult = 0;
+				parseInput(2,&YaNResult,NULL,YaN,NULL,"[y]es","[n]o");
+				if(YaNResult != 1)
+				{
+					puts("Player did not accept.");
+					return;
+				}
+				(players[selPla2])->money -= amount;
+				(properties[select])->owner = selPla2;
 			}
 			else
 			{
@@ -222,20 +231,20 @@ void trade(int current,struct player **players,struct property **properties,int 
 				while(1)
 				{
 					parseInput(1,NULL,&prop,NULL,range,NULL,NULL);
-					if((properties[prop-1]->owner != current)
+					if((properties[prop-1])->owner != current)
 					{
 						break;
 					}
-					if((properties[prop-1]->owner == -1)
+					if((properties[prop-1])->owner == -1)
 					{
 						puts("That property isn't owned.");
 						continue;
 					}
-					puts("You own that property.")	
+					puts("You own that property.");	
 				}
 				prop--;
 				printf("%s, do you want to receive this property %s, in exchange for %s?"
-				,(player[(properties[prop])->owner])->id,(properties[select])->name,(properties[prop])->name);
+				,(players[(properties[prop])->owner])->id,(properties[select])->name,(properties[prop])->name);
 				int answer = 0;
 				char want[2] = {'y','n'};
 				parseInput(2,&answer,NULL,want,NULL,"[y]es","[n]o");
@@ -296,7 +305,7 @@ void trade(int current,struct player **players,struct property **properties,int 
 				puts("Please enter a number above zero.");
 			}
 		}
-		if((player[current])->money < money)
+		if((players[current])->money < money)
 		{
 			puts("You don't have enough money.");
 			return;
@@ -315,8 +324,8 @@ void trade(int current,struct player **players,struct property **properties,int 
 		int range[2] = {1,amntPlayers};
 		parseInput(1,NULL,&selPla,NULL,range,NULL,NULL);
 		selPla--;
-		(player[current])->money -= money;
-		(player[selPla])->money =+ money;
+		(players[current])->money -= money;
+		(players[selPla])->money += money;
 	}			
 }
 
