8fc8ea0c412fad6a47463ab110f90b983fb2e920
Jordan W <jordanw2@gmail.com>
2011-01-14 23:12:27 +1030

    Added bankruptcy check

--- Monopoly.c
+++ Monopoly.c
@@ -51,6 +51,7 @@ void trade(int ,struct player **,struct property **,int ,int );
 void parseInput(int ,int *,int *,char *,int *,char *,char *);
 void updateStruct(struct property **,struct player **,int ,int ,int );
 void mortgagePrice(int *);
+void bankruptcy(struct property **,struct player **,int ,int ,int ,int *,int *);
 
 int main(void)
 {	
@@ -584,6 +585,7 @@ int gameLoop(struct property **properties,
 		puts("# You didn't enter in any players, exiting #");
 		exit(0);
 	}
+	int previous = -1;
 	for(currentPlayer = 0;;currentPlayer++)
 	{
 		if(currentPlayer >= amntPlayers)
@@ -615,8 +617,43 @@ int gameLoop(struct property **properties,
 		i = 0;
 		int action = queryPlayer((players[currentPlayer])->id);
 		actOnAction(properties,players,amntProperties,amntPlayers,corners,action,currentPlayer,urandom,doubles);
+		bankruptcy(properties,players,amntProperties,amntPlayers,currentPlayer,doubles,&previous);
 	}
 	return 0;
+}
+
+void bankruptcy(struct property **properties,
+				struct player **players,
+				int amntProperties,
+				int amntPlayers,
+				int currentPlayer,
+				int *doubles,
+				int *previous)
+{
+	if((players[currentPlayer])->money >= 0)
+	{
+		return;
+	}
+	if(*previous == currentPlayer)
+	{
+		playerResign(currentPlayer,players[currentPlayer],properties,amntProperties);
+		puts("\nYou have resigned for not clearing your debt in time.");
+		*doubles = 0;
+		return;
+	}
+	puts("\nYou currently have negative money,\nplease in the current turn achieve a positive money value,\n"
+	"if you don't you will automatically resign from the game.");
+	*doubles = 1;
+	*previous = currentPlayer;
+	puts("Press any button to continue.");
+	while(1)
+	{
+		char retr = getchar();
+		if(retr == '\n')
+		{
+			break;
+		}
+	}
 }
 
 void updateStruct(struct property **properties,struct player **players,int amntProperties,int amntPlayers,int currentPlayer)
