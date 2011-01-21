86bc095412ad25b77873feed83c8c24ccf1515d8
Jordan W <jordanw2@gmail.com>
2011-01-21 20:21:17 +1030

    board displaying properties is working, still more fine tuning needed

--- src/graphics.c
+++ src/graphics.c
@@ -69,35 +69,82 @@ static void createBoard(SDL_Surface **board,
 {
 	int i = 0;
 	int corners = 0;
-	//0 = x-x,1 = x-y,2 = x+x,3 = x-y
-	int mode = 0;
 	SDL_Rect position;
 	position.x = 0;
 	position.y = 0;
 	SDL_Rect cord;
 	cord.x = 0;
 	cord.y = 0;
-	for(i = 0;i < amntProperties;i++)
+	int first = 1;
+	int notNow = 0;
+	for(i = 0;i < amntProperties + SIDES;i++,notNow = 0)
 	{
 		if((properties[i])->type == 1)
 		{
 			corners++;
 		}
-		if(corners == 1)
+		if(corners == 1 && first == 1)
+		{
+			position.x = requirement.w * 2;
+			position.y = requirement.h;
+			first = 0;
+			notNow = 1;
+		}
+		if(corners == 1 && first == 0 && notNow == 0)
+		{
+			position.x += requirement.w;
+		}
+		if(corners == 2 && first == 1)
+		{
+			position.x += requirement.w;
+			position.y += requirement.h;
+			first = 0;
+			notNow = 1;
+		}
+		if(corners == 2 && first == 0 && notNow == 0)
+		{
+			position.y += requirement.h;
+		}
+		if(corners == 3 && first == 1)
+		{
+			position.x -= requirement.w;
+			position.y += requirement.h;
+			first = 0;
+			notNow = 1;
+		}
+		if(corners == 3 && first == 0 && notNow == 0)
+		{
+			position.x -= requirement.w;
+		}
+		if(corners == 4 && first == 1)
 		{
 			position.x = requirement.w;
-			position.y = requirement.h;
+			position.y -= requirement.h;
+			first = 0;
+			notNow = 1;
+		}
+		if(corners == 4 && first == 0 && notNow == 0)
+		{
+			position.y -= requirement.h;
+		}
+		if(i < amntProperties + SIDES - 1)
+		{
+			if((properties[i+1])->type == 1)
+			{
+				first = 1;
+			}
 		}
 		SDL_Surface *property;
 		generateProperty(&property,amntProperties,properties[i],players,requirement);
+			
 		cord.x = boardSize.w - position.x;
 		cord.y = boardSize.h - position.y;		
 		SDL_BlitSurface(property,NULL,*board,&cord);
-		position.x += requirement.w;
-		//DEBUG
-		/*printf("prop.w %d,prop.h %d,cord.x %d,cord.y %d,position.x %d,position.y %d,board.w %d,board.h %d\n",
-		property->w,property->h,cord.x,cord.y,position.x,position.y,boardSize.w,boardSize.h);*/
 		
+		
+		//DEBUG
+		/*printf("\nReq.w %d,Req.h %d,pos.x %d,pos.y %d,corn %d\n",requirement.w,requirement.h,position.x,position.y,corners);
+		printf("prop %d\n",amntProperties);*/
 		SDL_FreeSurface(property);	
 	}
 }
@@ -109,7 +156,14 @@ static void generateProperty(SDL_Surface **property,
 							 SDL_Rect requirement)
 {
 	*property = SDL_CreateRGBSurface(0,requirement.w,requirement.h,32,0,0,0,0);
-	SDL_FillRect(*property,NULL,SDL_MapRGB((*property)->format,255,255,255));
+	SDL_FillRect(*property,NULL,SDL_MapRGB((*property)->format,100,100,100));
+	SDL_Surface *innerProperty = SDL_CreateRGBSurface(0,requirement.w-2,requirement.h-2,32,0,0,0,0);
+	SDL_FillRect(innerProperty,NULL,SDL_MapRGB(innerProperty->format,255,255,255));
+	SDL_Rect innerOffset;
+	innerOffset.x = 1;
+	innerOffset.y = 1;
+	SDL_BlitSurface(innerProperty,NULL,*property,&innerOffset);
+	SDL_FreeSurface(innerProperty);
 	SDL_Rect offset;
 	offset.y = 5;
 	SDL_Surface *text = 0;
@@ -191,11 +245,12 @@ static void setBoardSize(struct property **properties,
 	int prop = 1;
 	int w = 0;
 	int h = 0;
-	for(i = 0,prop = 1;i < amntProperties;i++,prop++)
+	for(i = 0,prop = 1;i < amntProperties + SIDES;i++,prop++)
 	{
 		if((properties[i])->type == 1)
 		{
 			corners++;
+			prop = 1;
 		}
 		if(corners == 1 || corners == 3)
 		{
@@ -203,19 +258,22 @@ static void setBoardSize(struct property **properties,
 			{
 				w = prop;
 			}
-			prop = 1;
 		}
-		if(corners == 2 || corners || 4)
+		if(corners == 2 || corners == 4)
 		{
 			if(prop > h)
 			{
 				h = prop;
 			}
-			prop = 1;
 		}
 	}
-	boardSize->w = w * requirement.w + (requirement.h * 2);
-	boardSize->h = h * requirement.h + (requirement.w * 2);
+	boardSize->w = w * requirement.w + (requirement.w * 2);
+	boardSize->h = h * requirement.h + (requirement.h * 2);
+	//DEBUG
+	/*printf("Req.w %d,Req.h %d\n",requirement.w,requirement.h);
+	printf("Board.w %d,Board.h %d\n",boardSize->w,boardSize->h);
+	printf("w %d,h %d\n",w,h);*/
+	
 	*board = SDL_CreateRGBSurface(0,boardSize->w,boardSize->h,32,0,0,0,0);	
 }
 
